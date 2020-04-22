654258cb7f19f1cf43de0a17f5f467226294c3b5
vernike490 <crimhax76@haxmail.net>
2020-04-22 21:41:38 +0000

    Made whois and functions work

---
+++ commands/who is.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const { stripIndents } = require("common-tags");
+const { getMember, formatDate } = require("../functions.js");
+
+module.exports.help = {
+  name: 'whois',
+  aliases: ['user, info']//Leave Blank for no Aliases
+};
+
+
+module.exports.run = async (client, message, args) => {
+
+  const member = getMember(message, args.join(" "));
+
+  // Member variables
+  const joined = formatDate(member.joinedAt);
+  const roles = member.roles.cache
+    .filter(r => r.id !== message.guild.id)
+    .map(r => r).join(", ") || 'none';
+
+  // User variables
+  const created = formatDate(member.user.createdAt);
+
+  const embed = new Discord.MessageEmbed()
+    .setFooter(member.displayName, member.user.displayAvatarURL)
+    .setThumbnail(member.displayAvatarURL)
+    .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
+
+    .addField('Member information:', stripIndents`**- Display name:** ${member.displayName}
+            **- Joined at:** ${joined}
+            **- Roles:** ${roles}`, true)
+
+    .addField('User information:', stripIndents`**- ID:** ${member.user.id}
+            **- Username**: ${member.user.username}
+            **- Tag**: ${member.user.tag}
+            **- Created at**: ${created}`, true)
+
+    .setTimestamp();
+
+  if (member.user.presence.game)
+    embed.addField('Currently playing', stripIndents`** Name:** ${member.user.presence.game.name}`);
+
+  message.channel.send(embed);
+};
